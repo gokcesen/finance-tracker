df402709e452bf939e6861ed36c97fecafeabe4c
gokcesen <sengkce@gmail.com>
2025-01-30 12:50:26 +0100

    Add many-to-many association between users and stocks

--- finance_tracker/app/models/stock.rb
+++ finance_tracker/app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   require 'net/http'
   require 'json'
 
