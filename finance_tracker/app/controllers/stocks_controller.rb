f3b21295069dd26ef839ca98773b07305fe8aa02
gokcesen <sengkce@gmail.com>
2025-01-30 10:57:26 +0100

    Add ajax functionality for searching stocks and handling errors

--- finance_tracker/app/controllers/stocks_controller.rb
+++ finance_tracker/app/controllers/stocks_controller.rb
@@ -1,18 +1,25 @@
 class StocksController < ApplicationController
-  
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock.nil?
         flash.now[:alert] = "Invalid stock symbol"
-        render 'users/my_portfolio' and return
+        respond_to do |format|
+          format.html { render 'users/my_portfolio' }
+          format.turbo_stream { render turbo_stream: turbo_stream.replace("stock-info", partial: "shared/empty_stock") }
+        end
       else
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.html { render 'users/my_portfolio' }
+          format.turbo_stream { render turbo_stream: turbo_stream.replace("stock-info", partial: "users/result") }
+        end
       end
-    else 
+    else
       flash.now[:alert] = "Please enter a stock symbol"
-      render 'users/my_portfolio'
+      respond_to do |format|
+        format.html { render 'users/my_portfolio' }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("stock-info", partial: "shared/empty_stock") }
+      end
     end
   end
 end
-
