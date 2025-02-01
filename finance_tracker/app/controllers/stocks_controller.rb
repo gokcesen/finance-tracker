9e434b27b0dc104613ffd663b521309ab981c888
gokcesen <sengkce@gmail.com>
2025-02-01 11:33:42 +0100

    Add stock tracking restrictions for users

--- finance_tracker/app/controllers/stocks_controller.rb
+++ finance_tracker/app/controllers/stocks_controller.rb
@@ -2,37 +2,36 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      if @stock.nil?
-        flash.now[:alert] = "Invalid stock symbol"
-        respond_to do |format|
-          format.html { render 'users/my_portfolio' }
-          format.turbo_stream { render turbo_stream: turbo_stream.replace("stock-info", partial: "shared/empty_stock") }
+      
+      # Yeni: @tracked_stocks'u güncelle
+      @tracked_stocks = current_user.stocks
+
+      respond_to do |format|
+        format.turbo_stream do
+          if @stock.nil?
+            render turbo_stream: [
+              turbo_stream.replace("results", partial: "shared/empty_stock"),
+              turbo_stream.replace("tracked_stocks", partial: "stocks/empty_tracked_stocks")
+            ]
+          else
+            render turbo_stream: [
+              turbo_stream.replace("results", partial: "users/result"),
+              turbo_stream.replace("tracked_stocks", partial: "stocks/list")
+            ]
+          end
         end
-      else
-        respond_to do |format|
-          format.html { render 'users/my_portfolio' }
-          format.turbo_stream { render turbo_stream: turbo_stream.replace("stock-info", partial: "users/result") }
-        end
+        format.html { render 'users/my_portfolio' }
       end
     else
       flash.now[:alert] = "Please enter a stock symbol"
       respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("results", partial: "shared/empty_stock") }
         format.html { render 'users/my_portfolio' }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("stock-info", partial: "shared/empty_stock") }
       end
     end
   end
-
-  # def create
-  #   # Assuming you're adding the stock to the user's portfolio here
-  #   @stock = Stock.find_by(ticker: params[:ticker])
-  #   if @stock && current_user
-  #     # Add the stock to the user's portfolio (assuming you have a `has_many :stocks` relationship)
-  #     current_user.stocks << @stock
-  #     flash[:notice] = "Stock added to your portfolio"
-  #   else
-  #     flash[:alert] = "Could not add stock to portfolio"
-  #   end
-  #   redirect_to my_portfolio_path
-  # end
 end
+
+
+
+
