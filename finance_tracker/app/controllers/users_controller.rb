cd51c966ba40973d0a21bb98b13a5244dbf95d89
gokcesen <sengkce@gmail.com>
2025-02-02 10:21:11 +0100

    Add user search methods

--- finance_tracker/app/controllers/users_controller.rb
+++ finance_tracker/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
@@ -7,7 +11,30 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
-  def search 
-    render json: params[:friend]
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends.empty?
+        respond_to do |format|
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.update('friend-results', partial: 'users/friend_result')
+          end
+        end
+      else
+        respond_to do |format|
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.update('friend-results', partial: 'users/friend_result')
+          end
+        end
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update('friend-results', partial: 'users/friend_result')
+        end
+      end
+    end
   end
+  
 end
